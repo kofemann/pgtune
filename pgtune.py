9c00d51e207744803e0be6f82be28ae2ced88021
Tiramisu Mokka <kofemann@gmail.com>
2014-09-26 15:39:58 +0200

    do not use OrderedDict
     1. no need
     2. requires Python-2.7

--- pgtune.py
+++ pgtune.py
@@ -5,7 +5,6 @@ import string
 import getopt
 import sys
 from math import floor, log
-from collections import OrderedDict
 
 B = 1
 K = 1024
@@ -28,7 +27,7 @@ def get_size(s):
 
 
 def available_memory():
-    meminfo = OrderedDict()
+    meminfo = {}
     with open('/proc/meminfo') as f:
         for line in f:
             s = line.split(': ')
