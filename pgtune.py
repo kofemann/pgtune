7d88b4e11ae88e41e06049dc30ee06e8dc1f2e74
Tiramisu Mokka <kofemann@gmail.com>
2016-10-10 17:13:16 +0200

    disable statements stats by default
    just load the module, but do not collect statistics

--- pgtune.py
+++ pgtune.py
@@ -148,6 +148,7 @@ def main():
     print("listen_addresses = '%s'" % (listen_addresses))
     if enable_stat:
         print("shared_preload_libraries = 'pg_stat_statements'")
+        print("pg_stat_statements.track = none")
     
 
 if __name__ == '__main__':
